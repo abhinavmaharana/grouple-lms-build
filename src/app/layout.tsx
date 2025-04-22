24ad3f2e72055e776cb361b687b34e0221d85fb0
abhinavmaharana <abhinavmaharana.004@gmail.com>
2025-04-22 22:16:24 +0530

    fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs"
 import type { Metadata } from "next"
 import { Plus_Jakarta_Sans } from "next/font/google"
 import { Toaster } from "sonner"
-import "./global.css"
+import "./globals.css"
 
 const jakarta = Plus_Jakarta_Sans({ subsets: ["latin"] })
 
